6ed74e8559d83c31a52d300cc598f372eb7e1572
Kolabank <kolabank@gmail.com>
2022-08-16 08:47:20 +0100

    This is the first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const addButton = document.querySelector(".nameButton")
+console.dir(addButton);
+addButton.addEventListener("click", buttonClick)
+
+function buttonClick() {
+    alert("Button is clicked");
+
+}
